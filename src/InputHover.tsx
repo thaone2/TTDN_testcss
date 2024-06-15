5728df2a74262277fed15e4d043443bcd074699b
thaone2 <khuong123w@gmail.com>
2024-06-15 17:51:38 +0700

    update: update result v

--- src/InputHover.tsx
+++ src/InputHover.tsx
@@ -18,14 +18,14 @@ function InputHover() {
       <span
         className={`flex flex-col pl-4  ${
           hover || inputValue
-            ? "text-blue-400 -translate-x-3 duration-500  "
-            : "text-[#646F79] translate-x-3 duration-500"
+            ? "text-blue-400 -translate-x-3 duration-300  "
+            : "text-[#646F79] translate-x-3 duration-300"
         }`}
       >
         Username
       </span>
       <input
-        className={`bg-blue-400 border-t-2 pl-2 justify-center items-center flex border-blue-400 rounded-[8px] text-black focus:outline-none transition-all duration-700 ease-out ${
+        className={`bg-blue-400 border-t-2 pl-2 justify-center items-center flex border-blue-400 rounded-[8px] text-black focus:outline-none transition-all duration-300 ease-out ${
           hover || inputValue ? "h-12 p-2 opacity-100" : "h-0"
         }`}
         type="text"
